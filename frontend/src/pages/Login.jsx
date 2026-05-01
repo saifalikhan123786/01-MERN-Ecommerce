9cbaead6beea3c3495be013365c75ec3884c1a61
Saif Ali Khan <saifalikhan123@example.com>
2026-05-01 16:34:27 +0500

    login form complete

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,80 @@
+import { useState } from "react";
+import {useNavigate} from "react-router";
+import api from "../api/axios"
+
+
 export default function Login(){
+    const [form,setForm]=useState({
+        email:"",
+        password:""
+    })
+    const [msg,setMsg]=useState("");
+    const navigate=useNavigate();
+
+
+    const handleChange=(e)=>{
+       setForm({
+        ...form,
+        [e.target.name]:e.target.value
+       });
+    }
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        try{
+            const res= await api.post("/auth/login",form);
+             console.log(res,"data");
+            // Save Token to login storage
+            localStorage.setItem("token",res.data.token);
+
+            setMsg("Login Successfully");
+
+            // Redirect to Home page
+            setTimeout(()=>{
+                navigate("/");
+            },1000);
+
+        }
+        catch(err){
+            setMsg(err.response?.data?.message || "An error occured");
+
+        }
+    }
+
     return(
-        <div>
-          <h1>Welcom to the Login page</h1>  
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+          <h2 className="text-2xl font-bold mb-6 text-center">Login to Your Account</h2>
+          {msg && (
+            <div className="mb-4 text-center text-sm text-red-600 font-medium">
+                {msg}
+                </div>
+          )}
+            <form onSubmit={handleSubmit} className="space-y-4">
+            <input 
+            name='email'
+            type='email'
+            placeholder="Enter Email"
+            value={form.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+            required
+            />
+            <input 
+            name='password'
+            type='password'
+            placeholder="Enter password"
+            value={form.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+            required
+            />
+            <button 
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 "
+            >Login</button>
+            </form>
+        </div>
         </div>
     );
 }
